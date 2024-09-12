dc9256e4fe94823937ca58c393290494e14dcbd1
rayka999 <raykalgarcia07@gmail.com>
2024-09-12 15:46:44 -0300

    guigiug

---
+++ modulariedade/app.mjs
@@ -0,0 +1,4 @@
+import { saudacao } from './saudacao.js';// usa-se ./ se o arquivo estiver na mesma pasta
+
+console.log(saudacao("Carlos")); // Saída: Olá, Carlos!
+//usa-se ../ se o arquivo estiver na pasta de cima
