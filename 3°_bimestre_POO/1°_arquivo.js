0befe0b8be6758018b4ca378b7a4817922f7336f
rayka999 <raykalgarcia07@gmail.com>
2024-10-22 11:47:08 -0300

    type

---
+++ 3°_bimestre_POO/1°_arquivo.js
@@ -0,0 +1,13 @@
+/*TS é uma linguagem transpilada. A transpilação é um processo
+utilizado na engenharia de software que consiste em transformar
+o código fonte de um programa escrito em uma linguagem para
+o código fonte equivalente em outra linguagem*/
+/*O TypeScript adiciona tipagem explícita, o que ajuda a
+prevenir erros.*/
+var hello;
+hello = "Olá mundo";
+console.log(hello);
+function add(a, b) {
+    return a + b;
+}
+/*O TypeScript também pode inferir tipos automaticamente. */ 
---
+++ 3°_bimestre_POO/1°_arquivo.ts
@@ -0,0 +1,15 @@
+/*TS é uma linguagem transpilada. A transpilação é um processo
+utilizado na engenharia de software que consiste em transformar
+o código fonte de um programa escrito em uma linguagem para 
+o código fonte equivalente em outra linguagem*/
+/*O TypeScript adiciona tipagem explícita, o que ajuda a 
+prevenir erros.*/
+let hello: string;
+hello="Olá mundo";
+console.log(hello);
+
+function add(a: number, b: number): number {
+    return a + b;
+  }
+
+/*O TypeScript também pode inferir tipos automaticamente. */
