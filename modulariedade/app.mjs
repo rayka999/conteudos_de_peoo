aa6ff6dda9d646f59850aa0524138c6e33d00b49
rayka999 <raykalgarcia07@gmail.com>
2024-12-02 08:25:03 -0300

    Delete modulariedade/app.mjs
    jbhib

--- modulariedade/app.mjs
+++
@@ -1,4 +0,0 @@
-import { saudacao } from './saudacao.mjs';// usa-se ./ se o arquivo estiver na mesma pasta
-
-console.log(saudacao("João")); // Saída: Olá, Carlos!
-//usa-se ../ se o arquivo estiver na pasta de cima
